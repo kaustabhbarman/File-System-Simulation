00d158176776de4e6f907db5dcaf07d47debf67e
kaustabhbarman <kaustabh.barman13@gmail.com>
2016-04-09 22:19:59 +0530

    First Commit
    Only container created as a text file

---
+++ FileSystemSimulation/src/filesystemsimulation/FileSystemSimulation.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesystemsimulation;
+
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author kaustabh
+ */
+public class FileSystemSimulation {
+    
+    public void writefile(){
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter: ");
+        String t = in.nextLine();
+        System.out.println(t);
+    }
+    
+}
